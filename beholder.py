07f4d63d2a2e8613c5870b20cbeaef2ce6aafab0
13oz <duminsky.nick@gmail.com>
2018-03-06 22:52:12 +0300

    change structure of project

--- beholder.py
+++ beholder.py
@@ -1,14 +1,13 @@
-#!/usr/bin/python
+#!/usr/local/bin/python3
 
 import argparse
 
 parser = argparse.ArgumentParser(description='Process arguments...')
-parser.add_argument('--api_key', type='str', help='Add your SHODAN API key to connect to SHODAN search engine')
-parser.add_argument('--config_file', help='Add search config file')
 
 
 def connectShodan(api_key='foo'):
 	return 0
 
 if __name__ == "__main__":
-	connectShodan
+	args = parser.parse_args()
+	print(args.api)
